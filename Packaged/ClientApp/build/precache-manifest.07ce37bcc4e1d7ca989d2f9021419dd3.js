dcd1161f89704e334032be7ff8e2e7eb696d98d8
46016z <46016z@unibit.bg>
2021-11-02 21:24:16 +0200

    Adding packaged source"

---
+++ Packaged/ClientApp/build/precache-manifest.07ce37bcc4e1d7ca989d2f9021419dd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f2764e089c7f8cedbbc831c4c7f7af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c05518fabebf4d7d94a2",
+    "url": "/static/css/2.58c793c3.chunk.css"
+  },
+  {
+    "revision": "b84898ffca097e1875c1",
+    "url": "/static/css/main.0de07c89.chunk.css"
+  },
+  {
+    "revision": "c05518fabebf4d7d94a2",
+    "url": "/static/js/2.a5a152ff.chunk.js"
+  },
+  {
+    "revision": "0377c71fe133483615390c98f30c0111",
+    "url": "/static/js/2.a5a152ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84898ffca097e1875c1",
+    "url": "/static/js/main.27e7f5d9.chunk.js"
+  },
+  {
+    "revision": "c6c698e0e65f2cb55189",
+    "url": "/static/js/runtime-main.cd4a4b2b.js"
+  }
+]);
